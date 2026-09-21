a7353b61199b580cc6cb499fb197505de0a5eccf
Erickson Salangsang <sonsonyyy20@gmail.com>
2026-09-21 17:18:04 +0800

    feat: add episodes, topics, and config modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EpisodesModule } from './episodes/episodes.module';
+import { TopicsModule } from './topics/topics.module';
 
 @Module({
-  imports: [],
+  imports: [EpisodesModule, TopicsModule],
   controllers: [AppController],
   providers: [AppService],
 })
