653b68c8a8d601707a0613115f287d413e8cca50
Erickson Salangsang <sonsonyyy20@gmail.com>
2026-09-21 23:18:20 +0800

    feat: example dependency injection from config module to episode module

---
+++ src/config/config.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ConfigService {
+    async logMessage() {
+        console.log('This is from the config service class');
+    }
+}
--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -1,30 +1,38 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { EpisodesService } from './episodes.service';
 import { CreateEpisodeDto } from './dto/create-episode.dto';
+import { ConfigService } from 'src/config/config.service';
 
 @Controller('episodes')
 export class EpisodesController {
-    constructor(private episodeService: EpisodesService) {}
+    constructor(
+        private episodeService: EpisodesService,
+        private configService: ConfigService
+    ) {}
 
     @Get()
     findAll(@Query('sort') sort: 'asc' | 'desc' = 'desc') {
         console.log(sort);
+        this.configService.logMessage();
         return this.episodeService.findAll(sort);
     }
 
     @Get('featured')
     findFeatured() {
+        this.configService.logMessage();
         return this.episodeService.findFeatured();
     }
 
     @Get(':id')
     findOne(@Param() id: string) {
         console.log(id);
+        this.configService.logMessage();
         return this.episodeService.findOne(id);
     }
 
     @Post()
     create(@Body() input: CreateEpisodeDto) {
+        this.configService.logMessage();
         return this.episodeService.create(input);
     }
 }
