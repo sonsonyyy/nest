3c1a85e56249101622755e1c515e5dd04d940f4c
Erickson Salangsang <sonsonyyy20@gmail.com>
2026-09-21 22:57:47 +0800

    feat: add episode provider class (service class), entity, and dto. Inject service class to controller

--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -1,26 +1,30 @@
-import { Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { EpisodesService } from './episodes.service';
+import { CreateEpisodeDto } from './dto/create-episode.dto';
 
 @Controller('episodes')
 export class EpisodesController {
+    constructor(private episodeService: EpisodesService) {}
+
     @Get()
     findAll(@Query('sort') sort: 'asc' | 'desc' = 'desc') {
         console.log(sort);
-        return 'all episodes';
+        return this.episodeService.findAll(sort);
     }
 
     @Get('featured')
     findFeatured() {
-        return 'featured episodes';
+        return this.episodeService.findFeatured();
     }
 
     @Get(':id')
     findOne(@Param() id: string) {
         console.log(id);
-        return 'one episode';
+        return this.episodeService.findOne(id);
     }
 
     @Post()
-    create() {
-        return 'new episode';
+    create(@Body() input: CreateEpisodeDto) {
+        return this.episodeService.create(input);
     }
 }
